29259bebd54fc9a6eb97b2a746e30ed85dcfbc76
TheOdinblade <50718347+TheOdinblade@users.noreply.github.com>
2020-06-26 10:42:47 +0100

    v0.6.1
    Code cleanup

--- Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapServer.lua
@@ -4,13 +4,10 @@ end
 
 Ext.Require("Shared/OdinSCO_Utility.lua")
 Ext.Require("Server/OdinSCO_ComboSkills.lua")
-Ext.Require("Server/OdinSCO_CooldownManager.lua")
-
 Ext.Require("Shared/OdinSCO_ComboPoints.lua")
 
 local ModuleLoading = function ()
     Ext.Print("[OdinSCO:BootstrapServer.lua] Module is loading.")
-    -- Osi.DB_OdinSCO_SkillCooldown("NULL_00000000-0000-0000-0000-000000000000", "", 0.0)
     OverrideStats()
 end
 
--- Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/Shared/OdinSCO_ComboPoints.lua
+++ Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/Shared/OdinSCO_ComboPoints.lua
@@ -31,7 +31,7 @@ function incrementComboPoints(character, points)
                 if HasActiveStatus(character, "OdinSCO_COMBO_3") == 0 then
                     ApplyStatus(character, "OdinSCO_COMBO_3", -1.0, 1, character)
                     SetTag(character, "ODINSCO_COMBO_READY")
-                    refreshDeadlyFlourish(character) --TOOD: Try removing this - might work as intended outside of the engine?
+                    refreshDeadlyFlourish(character)
                 end
             end
         end
@@ -48,36 +48,6 @@ local function enterCombatComboBonus(character)
         end
     end
 end
-
--- -- Transforms skills as the player moves through combo tiers
--- local function setComboTierSkills(character, points, prevPoints)
---     for a, comboSkill in pairs(OdinScoundrelOverhaul.ComboSkills) do
---         local newSkill = comboSkill[points]
---         if newSkill ~= nil then
---             local prevSkill = comboSkill[prevPoints]
---             -- if points == 0 then
---             --     prevSkill = comboSkill[prevPoints]
---             -- end
---             if CharacterHasSkill(character, prevSkill) == 1 then
---                 local skillIsLearned = NRD_SkillGetInt(character, prevSkill, "IsLearned")
---                 Ext.Print("skillIsLearned: "..skillIsLearned.." prevSkill: "..prevSkill)
---                 local isActivated = NRD_SkillGetInt(character, prevSkill, "IsActivated")
---                 Ext.Print("skillIsLearned: "..isActivated.." prevSkill: "..prevSkill)
---                 local zeroMemory = NRD_SkillGetInt(character, prevSkill, "ZeroMemory")
---                 Ext.Print("skillIsLearned: "..zeroMemory.." prevSkill: "..prevSkill)
---                 if skillIsLearned == 1 and prevSkill ~= newSkill then
---                     local prevSlot = NRD_SkillBarFindSkill(character, prevSkill)
---                     local prevCD = NRD_SkillGetCooldown(character, prevSkill)
-                    
---                     CharacterRemoveSkill(character, prevSkill)
---                     CharacterAddSkill(character, newSkill, 0)
---                     NRD_SkillBarSetSkill(character, prevSlot, newSkill)
---                     NRD_SkillSetCooldown(character, newSkill, prevCD)
---                 end
---             end
---         end
---     end
--- end
 
 local function activateComboPoints(character)
     local comboPointsCount = getComboPoints(character)
@@ -121,77 +91,10 @@ function consumeComboPoints(character)
     RemoveStatus(character, "OdinSCO_USE_COMBO_1")
 
     ClearTag(character, "ODINSCO_COMBO_READY")
-    refreshDeadlyFlourish(character) --TOOD: Try removing this - might work as intended outside of the engine?
+    refreshDeadlyFlourish(character)
 end
-
--- function consumeComboPoints(character, activatedComboPoints, comboToAdd)
---     local activatedComboPointsCount = getActivatedComboPoints(character)
---     local comboPointsStatus = nil
---     for status, value in pairs(OdinScoundrelOverhaul.ComboPoints) do
---         if HasActiveStatus(character, status) == 1 then
---             comboPointsStatus = status
---         end
---     end
---     local activatedCombo = nil
---     for status, value in pairs(OdinScoundrelOverhaul.ActivatedComboPoints) do
---         if value == activatedComboPoints then
---             activatedCombo = status
---         end
---     end
---     if comboPointsStatus ~= nil and activatedCombo ~= nil then
---         local comboPointsToRemove = OdinScoundrelOverhaul.ActivatedComboPoints[activatedCombo]
---         local comboPointsCount = OdinScoundrelOverhaul.ComboPoints[comboPointsStatus]
---         local newComboPointsCount = comboPointsCount - comboPointsToRemove
-
---         RemoveStatus(character, "OdinSCO_COMBO_3")
---         RemoveStatus(character, "OdinSCO_COMBO_2")
---         RemoveStatus(character, "OdinSCO_COMBO_1")
-
---         RemoveStatus(character, "OdinSCO_USE_COMBO_3")
---         RemoveStatus(character, "OdinSCO_USE_COMBO_2")
---         RemoveStatus(character, "OdinSCO_USE_COMBO_1")
-
---         if comboToAdd > 0 then
---             newComboPointsCount = newComboPointsCount + comboToAdd
---         end
-
---         incrementComboPoints(character, newComboPointsCount)
---         -- Ext.Print("Setting comboTier to: "..newComboPointsCount.." with "..activatedComboPointsCount)
---         -- setComboTierSkills(character, newComboPointsCount, activatedComboPointsCount)
---     else
---         -- setComboTierSkills(character, 0, 0)
---     end
--- end
-
--- function checkAddedSkill(character, skillId)
---     Ext.Print("Check added skill entered")
-
---     if NRD_SkillGetInt(character, skillId, "IsLearned") == 0 then
---         Ext.Print("I think dis is a weapon skill: "..skillId)
---     end
-
---     if NRD_SkillGetInt(character, skillId, "ZeroMemory") == 1 then
---         Ext.Print("ZERO MEM")
---     end
-
-
---     local comboSkillMatch = OdinScoundrelOverhaul.ComboSkills[skillId]
---     if comboSkillMatch ~= nil then
---         for tier, skill in pairs(comboSkillMatch) do
---             if CharacterHasSkill(character, skill) == 1 then
---                 Ext.Print("aaaah")
---                 local skillIsLearned = NRD_SkillGetInt(character, skill, "IsLearned")
---                 if skillIsLearned == 1 then
---                     Ext.Print("abc")
---                     CharacterRemoveSkill(character, skillId)
---                 end
---             end
---         end
---     end
--- end
 
 Ext.NewCall(incrementComboPoints, "OBSCO_LUA_IncrementComboPoints", "(CHARACTERGUID)_Character, (INTEGER)_PointsToAdd");
 Ext.NewCall(activateComboPoints, "OBSCO_LUA_ActivateComboPoints", "(CHARACTERGUID)_Character");
 Ext.NewCall(consumeComboPoints, "OBSCO_LUA_ConsumeComboPoints", "(CHARACTERGUID)_Character");
 Ext.NewCall(enterCombatComboBonus, "OBSCO_LUA_EnterCombatComboPoints", "(CHARACTERGUID)_Character");
--- Ext.NewCall(checkAddedSkill, "OBSCO_LUA_CheckAddedSkill", "(CHARACTERGUID)_Character, (STRING)_SkillId");
