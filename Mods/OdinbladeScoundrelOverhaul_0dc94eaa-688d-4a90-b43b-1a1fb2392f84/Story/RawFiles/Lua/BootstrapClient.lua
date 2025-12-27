f2c736b27ec4267c5f9b80b23e2827ae1a46cef5
Odinblade <50718347+Odinblade@users.noreply.github.com>
2025-12-27 12:41:04 +0000

    Resolve Bootstrap deprecation warnings

--- Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapClient.lua
@@ -1,13 +1,10 @@
-local function SessionLoading()
-    Ext.Print("[OdinSCO:BootstrapClient.lua] Session is loading.")
-end
-
 Ext.Require("Shared/OdinSCO_StatOverrides.lua")
 
-local ModuleLoading = function ()
-    Ext.Print("[OdinSCO:BootstrapClient.lua] Module is loading.")
-    OverrideStats()
-end
+Ext.Events.SessionLoading:Subscribe(function (e)
+    print("[OdinSCO:BootstrapClient.lua] Session is loading.")
+end)
 
-Ext.RegisterListener("ModuleLoading", ModuleLoading)
-Ext.RegisterListener("SessionLoading", SessionLoading)
+Ext.Events.StatsLoaded:Subscribe(function (e)
+    print("[OdinSCO:BootstrapClient.lua] StatsLoaded is loading.")
+    OverrideStats()
+end)
--- Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/OdinbladeScoundrelOverhaul_0dc94eaa-688d-4a90-b43b-1a1fb2392f84/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,15 +1,13 @@
-local function SessionLoading()
-    Ext.Print("[OdinSCO:BootstrapServer.lua] Session is loading.")
-end
-
 Ext.Require("Shared/OdinSCO_Utility.lua")
 Ext.Require("Server/OdinSCO_ComboSkills.lua")
 Ext.Require("Shared/OdinSCO_ComboPoints.lua")
+Ext.Require("Shared/OdinSCO_StatOverrides.lua")
 
-local ModuleLoading = function ()
-    Ext.Print("[OdinSCO:BootstrapServer.lua] Module is loading.")
+Ext.Events.SessionLoading:Subscribe(function (e)
+    print("[OdinSCO:BootstrapServer.lua] Session is loading.")
+end)
+
+Ext.Events.StatsLoaded:Subscribe(function (e)
+    print("[OdinSCO:BootstrapServer.lua] StatsLoaded is loading.")
     OverrideStats()
-end
-
-Ext.RegisterListener("ModuleLoading", ModuleLoading)
-Ext.RegisterListener("SessionLoading", SessionLoading)
+end)
